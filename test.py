22c768d74370426d21af167dc43bdb28bce1fb65
johnkao <liberbay105@gmail.com>
2023-12-26 22:43:21 +0900

    game

--- test.py
+++ test.py
@@ -2,3 +2,15 @@ import random
 
 # 1~50
 x = random.randint(1, 50)
+for i in range(5):
+    y = int(input("your 答え："))
+
+    if x == y:
+        print("YES!!GOOD!! LOVE AND PICE")
+        break
+    else:
+        print("ON~NO!!")
+        if x > y:
+            print("big")
+        else:
+            print("すも")
