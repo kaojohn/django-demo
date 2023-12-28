948179cf974403eadcf93950ad3e2997ccaca20a
johnkao <liberbay105@gmail.com>
2023-12-28 21:27:45 +0900

    1. test2_1228 john

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import random
 
 # 1~50
-x = random.randint(1, 50)
+
+start = 1
+end = 50
+
+
+x = random.randint(start, end)
+
 for i in range(5):
-    y = int(input("your 答え："))
+    y = int(input(f"find number {start}~{end} your 答え："))
 
     if x == y:
         print("YES!!GOOD!! LOVE AND PICE")
@@ -12,5 +18,11 @@ for i in range(5):
         print("ON~NO!!")
         if x > y:
             print("big!!")
+            if start < y:
+                start = y + 1
         else:
+            if end > y:
+                end = y - 1
             print("すも!!")
+if i == 4:
+    print(f"ans:{x}")
